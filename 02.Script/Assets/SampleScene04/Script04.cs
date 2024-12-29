a8292edc2133fb7d39836ab57e0446408ea460f6
CHOIJIONE\choij <choijione02@gmail.com>
2024-12-29 09:20:11 +0900

    02.Script 부분 실습 코드 추가

---
+++ 02.Script/Assets/SampleScene04/Script04.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script04 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string name;
+        name = "Sera";
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 02.Script/Assets/SampleScene16/Script16.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script16 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = 1;
+        if(x == 1)
+        {
+            int y = 2;
+            Debug.Log(x);
+            Debug.Log(y);
+        }
+        //Debug.Log(y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
